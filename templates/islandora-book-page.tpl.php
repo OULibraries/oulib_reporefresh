e3db43b34020f6dd37df85ad52b86d23dd3d885b
Stephanie <stephaniegallo@ou.edu>
2023-10-04 17:14:44 -0500

    created book-page template override for download button

---
+++ templates/islandora-book-page.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * Template file to style output.
+ */
+?>
+<?php
+  print $book_object_id ? l(t('Return to Book View'), "islandora/object/{$book_object_id}") : t('Orphaned page (no associated book)');
+?>
+<?php if (isset($viewer)): ?>
+  <div id="book-page-viewer">
+    <?php print $viewer; ?>
+  </div>
+<?php elseif (isset($object['JPG']) && islandora_datastream_access(ISLANDORA_VIEW_OBJECTS, $object['JPG'])): ?>
+  <div id="book-page-image">
+    <?php
+      $params = array(
+        'path' => url("islandora/object/{$object->id}/datastream/JPG/view"),
+        'attributes' => array(),
+      );
+      print theme('image', $params);
+    ?>
+  </div>
+<?php endif; ?>
+
+<?php 
+    if (module_exists('islandora_download_button')) {
+      $block = module_invoke('islandora_download_button', 'block_view', 'islandora_download_button');
+      print render($block['content']);
+    }
+?>
+
+<!-- @todo Add table of metadata values -->
